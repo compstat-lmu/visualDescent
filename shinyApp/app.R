0ff127173c1ae9608f2719b12e1bf1dd1df558c8
PhilippScheller <philipp-scheller@gmx.de>
2019-11-13 22:24:19 +0100

    shinyApp update various function selection; box constraint default plot range

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,6 +1,12 @@
-library(smoof)
 library(shiny)
 library(visualDescent)
+
+# Define general parameters of functions
+funData = readRDS("funData.rda")
+funNames = as.list(names(funData))
+xValues = lapply(funData, function(x) {lower = getLowerBoxConstraints(x)
+                                        upper = getUpperBoxConstraints(x)
+                                        return(c(lower = lower, upper = upper))})
 
 
 ui <- fluidPage(
@@ -11,42 +17,47 @@ ui <- fluidPage(
                       sidebarPanel(
                         selectInput("method", "Choose optimization method", choices = c("Gradien Descent", "Momentum",
                                                                           "AdaGrad", "Adam"), selected = "AdaGrad"),
-                        selectInput("startx1", "Choose start point x1", choices = round(seq(-4, 4, by = 0.1), 2),
-                                    selected = 0),
-                        selectInput("startx2", "Choose start point x2", choices = round(seq(-4, 4, by = 0.1), 2),
-                                    selected = -4),
-                        selectInput("functions", "Choose predefined function", choices = c("Function 1", "Function 2"),
-                                    selected = "Function 2"),
+                        uiOutput("x1coords"),
+                        uiOutput("x2coords"),
+                        selectInput("functions", "Choose predefined function", choices = funNames, selected = "Ackley2d"),
+
                         sliderInput("step.size", "Step size:", min = 0, max = 1, step = 0.05, value= 0.1),
-                        sliderInput("max.iter", "Maximum number of iterations:",min = 0, max = 200, step=1, value= 10),
+                        sliderInput("max.iter", "Maximum number of iterations:",min = 0, max = 1000, step=1, value= 10),
                         conditionalPanel(condition = "input.method == 'Momentum'",
                                           sliderInput("phi", HTML("Velocity parameter &Phi;:"),min = 0, max = 1,
                                                       step=0.05, value= 0.1)),
                         conditionalPanel(condition = "input.method == 'Adam'",
                                          sliderInput("phi1", HTML("Decay rate &Phi; 1:"),min = 0, max = 1,
-                                                     step=0.05, value= 0.5)),
+                                                     step=0.05, value= 0.9)),
                         conditionalPanel(condition = "input.method == 'Adam'",
                                          sliderInput("phi2", HTML("Decay rate &Phi; 2:"),min = 0, max = 1,
-                                                     step=0.05, value= 0.8)),
-                        uiOutput("Params")),
+                                                     step=0.05, value= 0.95))
+                        ),
 
         mainPanel(column(2, plotOutput("plot", width = "700px", height = "500px"))))
       )
 
 
-readRDS("funData.rds")
+
 
 server <- function(input, output, session){
 
   Reactives = reactiveValues()
 
   observe({
-    if(input$functions == "Function 1"){
-      Reactives$plot <<- funData$Ackley2d
-    }
-    if(input$functions == "Function 2"){
-      Reactives$plot <<- funData$Beale
-    }})
+  Reactives$plot <<- get(input$functions, funData)
+  })
+
+  output$x1coords = renderUI({ selectInput("startx1", "Choose start point x1",
+                                          choices = round(seq(get(input$functions, xValues)[1],
+                                                              get(input$functions, xValues)[3],
+                                                              length.out = 40), 2),
+                                          selected = 0)})
+  output$x2coords = renderUI({ selectInput("startx2", "Choose start point x2",
+                                           choices = round(seq(get(input$functions, xValues)[2],
+                                                               get(input$functions, xValues)[4],
+                                                               length.out = 40), 2),
+                                           selected = 0)})
 
   observe({
     Reactives$x1 <<- as.numeric(input$startx1)
@@ -61,25 +72,29 @@ server <- function(input, output, session){
   observe({
     if(input$method == "Gradien Descent"){
             output$plot = renderPlot({
-                            plot2d(Reactives$plot, -4, 4, -4, 4, 30,
+                            plot2d(Reactives$plot, get(input$functions, xValues)[1], get(input$functions, xValues)[3],
+                                   get(input$functions, xValues)[2], get(input$functions, xValues)[4], 30,
                                     gradDescent(Reactives$plot, c(Reactives$x1, Reactives$x2), step.size = input$step.size,
                                                           max.iter = input$max.iter)$results)})
     }
     if(input$method == "Momentum"){
       output$plot = renderPlot({
-        plot2d(Reactives$plot, -4, 4, -4, 4, 30,
+        plot2d(Reactives$plot, get(input$functions, xValues)[1], get(input$functions, xValues)[3],
+               get(input$functions, xValues)[2], get(input$functions, xValues)[4], 30,
                gradDescentMomentum(Reactives$plot, c(Reactives$x1, Reactives$x2), step.size = input$step.size,
                            max.iter = input$max.iter, phi = input$phi)$results)})
     }
     if(input$method == "AdaGrad"){
       output$plot = renderPlot({
-        plot2d(Reactives$plot, -4, 4, -4, 4, 30,
+        plot2d(Reactives$plot, get(input$functions, xValues)[1], get(input$functions, xValues)[3],
+               get(input$functions, xValues)[2], get(input$functions, xValues)[4], 30,
                adaGrad(Reactives$plot, c(Reactives$x1, Reactives$x2), step.size = input$step.size,
                                    max.iter = input$max.iter)$results)})
     }
     if(input$method == "Adam"){
       output$plot = renderPlot({
-        plot2d(Reactives$plot, -4, 4, -4, 4, 30,
+        plot2d(Reactives$plot, get(input$functions, xValues)[1], get(input$functions, xValues)[3],
+               get(input$functions, xValues)[2], get(input$functions, xValues)[4], 30,
                adam(Reactives$plot, c(Reactives$x1, Reactives$x2), step.size = input$step.size,
                        max.iter = input$max.iter, phi1 = Reactives$phi1, phi2 = Reactives$phi2)$results)})
     }})
