e9aeee41945c5d1c175e2db365719e5674894099
juliambr <ju.moosbauer@googlemail.com>
2019-11-28 15:26:02 +0100

    improve plots

--- R/plot.2d.R
+++ R/plot.2d.R
@@ -20,7 +20,7 @@
 #' @param algoNamw list of strings with names of algorithms. Length must equal length of 'xmat'.
 #'
 #' @export
-plot2d = function(f, x1.lower, x1.upper, x2.lower, x2.upper, n.x = 30L, xmat, trueOpt = NULL, algoName = NULL) {
+plot2d = function(f, x1.lower, x1.upper, x2.lower, x2.upper, n.x = 30L, xmat, trueOpt = NULL, algoName = NULL, plotSegments = FALSE) {
 
   # Check several input parameter
     if (checkList(xmat) == FALSE) stop("'xmat' must be a list object")
@@ -49,7 +49,9 @@ plot2d = function(f, x1.lower, x1.upper, x2.lower, x2.upper, n.x = 30L, xmat, tr
 
     if (!is.null(trueOpt)) plot.dfOpt = data.frame(x1 = trueOpt[1], x2 = trueOpt[2])
 
-
+  new_scale <- function(new_aes) {
+     structure(ggplot2::standardise_aes_names(new_aes), class = "new_aes")
+  }
 
   # Generate grid with specfied dimensions and data.frame for plot
   x1 = seq(x1.lower, x1.upper, length.out = n.x)
@@ -69,12 +71,15 @@ plot2d = function(f, x1.lower, x1.upper, x2.lower, x2.upper, n.x = 30L, xmat, tr
   plot.points.df = do.call(rbind, plot.points.df)
 
   # Generate contour plot and add steps of descent procedure as points
-  plot = ggplot(plot.df, aes(x1, x2, z = y)) +
-    stat_contour(bins = n.x, aes(colour = stat(level))) +
+  plot = ggplot(plot.df, aes(x1, x2)) +
+    stat_contour(bins = n.x, aes(z = y, color = stat(level))) +
     ggtitle(paste(plot.title)) +
-    geom_point(data = plot.points.df, aes(x = x1, y = x2, fill = name),
-               color = plot.points.df$col, alpha = .5) +
-    scale_fill_manual(name = "Test", values = plot.points.df$col) +
+    new_scale_color() +
+    geom_point(data = plot.points.df, aes(x = x1, y = x2, color = name),
+               alpha = .5) +
+    geom_path(data = plot.points.df, aes(x = x1, y = x2, color = name)) + 
+    # scale_fill_manual(name = "Test", values = plot.points.df$col) +    # geom_path(data = plot.points.df, aes(x = x1, y = x2, col = name)) + 
+    xlab(expression(theta[1])) + ylab(expression(theta[2])) + 
     # guides(colour = guide_legend(override.aes = list(color = c("black", "blue", "green", "grey")))) +
     # scale_colour_manual(breaks = plot.points.df$name, values = unique(plot.points.df$col)) +
     theme(plot.title = element_text(hjust = .5)) +
@@ -82,7 +87,7 @@ plot2d = function(f, x1.lower, x1.upper, x2.lower, x2.upper, n.x = 30L, xmat, tr
 
 
     if(!is.null(trueOpt)) {
-      plot = plot + geom_point(aes(x = plot.dfOpt$x1, y = plot.dfOpt$x2, fill = "True Optimum"), color = "purple")
+      plot = plot + geom_point(aes(x = plot.dfOpt$x1, y = plot.dfOpt$x2, fill = "Global Optimum"))
     }
   # for (i in 1:nresults) {
   #
